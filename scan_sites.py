212356ad4fd6cf74c08a4d71122e2df26120f7c7
Jorge Alberto Díaz Orozco <diazorozcoj@gmail.com>
2017-02-17 18:36:46 -0500

    modified to run forever until KeyboardInterrupt

--- scan_sites.py
+++ scan_sites.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NotiWeb.settings')
 import django
@@ -8,5 +9,15 @@ django.setup()
 from RSSReader.models import Sitio, scan
 
 if __name__ == '__main__':
-    for sitio in Sitio.objects.all():
-        scan(sitio)
+    while 1:
+        try:
+            for sitio in Sitio.objects.all():
+                try:
+                    scan(sitio)
+                except:
+                    pass
+        except KeyboardInterrupt:
+            break
+
+        sleep(300)
+
