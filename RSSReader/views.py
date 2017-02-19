1b2b4dbe1984776de51a387a1e00cb1e9f2cd66e
Jorge Alberto Díaz Orozco <diazorozcoj@gmail.com>
2017-02-19 02:46:46 -0500

    - back to 5

--- RSSReader/views.py
+++ RSSReader/views.py
@@ -7,6 +7,6 @@ def index(request):
     sitios = Sitio.objects.filter(activo=True).order_by('?')
     feeds = []
     for sitio in sitios:
-        feeds.extend(Entrada.objects.filter(sitio=sitio).order_by('-timestamp')[:10])
+        feeds.extend(Entrada.objects.filter(sitio=sitio).order_by('-timestamp')[:5])
 
     return render(request, 'index.html', {'sitios': sitios, 'feeds': feeds, 'anuncios': Anuncio.objects.all()})
